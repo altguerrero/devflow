2c76ef06a4d9927188bf09cc951d18bd92ea4a0c
Jose Guerrero <josealberto.guerreroayala@gmail.com>
2026-02-09 16:36:31 -0500

    chore(config): configure tailwind, prettier, and vscode settings

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 const Page = () => {
-  return <div>Start</div>;
+  return (
+    <main className="p-8">
+      <h1 className="mb-4 text-4xl font-bold">Titulo con Inter</h1>
+      <h2 className="font-space-grotesk text-4xl font-bold">Titulo con Space Grotesk</h2>
+    </main>
+  );
 };
 
 export default Page;
