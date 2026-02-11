0bd89102d68da0f27d7061f57287dc756232523b
Josè Guerrero <42391887+altguerrero@users.noreply.github.com>
2026-02-11 16:59:23 -0500

    chore(merge): #2 from altguerrero/feature/mobile-navigation
    feat: mobile navigation and basic route scaffolding

---
+++ constants/index.ts
@@ -0,0 +1,31 @@
+import ROUTES from "./routes";
+
+export const sidebarLinks = [
+  {
+    name: "Home",
+    href: ROUTES.HOME,
+    imgURL: "/icons/home.svg",
+  },
+  {
+    name: "Community",
+    href: ROUTES.COMMUNITY,
+    imgURL: "/icons/users.svg",
+  },
+  {
+    name: "Collections",
+    href: ROUTES.COLLECTIONS,
+    imgURL: "/icons/star.svg",
+  },
+  {
+    name: "Find Jobs",
+    href: ROUTES.JOBS,
+    imgURL: "/icons/suitcase.svg",
+  },
+  {
+    name: "Tags",
+    href: ROUTES.TAGS,
+    imgURL: "/icons/tag.svg",
+  },
+  { name: "Profile", href: ROUTES.PROFILE, imgURL: "/icons/user.svg" },
+  { name: "Ask a Question", href: ROUTES.ASK_QUESTION, imgURL: "/icons/question.svg" },
+];
