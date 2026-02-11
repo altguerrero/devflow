a4caef1b4dbbd852d792aa7679c736589d017261
Josè Guerrero <42391887+altguerrero@users.noreply.github.com>
2026-02-11 14:49:29 -0500

    chore(merge): #1 from altguerrero/feature/authentication
    Feature/authentication

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import AuthForm from "@/components/forms/AuthForm";
+import { SignInSchema } from "@/lib/validations";
+
+const SignIn = () => {
+  return (
+    <AuthForm
+      schema={SignInSchema}
+      defaultValues={{
+        email: "",
+        password: "",
+      }}
+      onSubmit={async (data) => {
+        void data;
+        return { success: false };
+      }}
+      formType={"SIGN_IN"}
+    />
+  );
+};
+
+export default SignIn;
--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import Theme from "./Theme";
+import ROUTES from "@/constants/routes";
 
 const Navbar = () => {
   return (
     <nav className="flex-between background-light900_dark200 fixed z-50 w-full p-6 sm:px-12 dark:shadow-none">
-      <Link href="/" className="flex items-center gap-1">
+      <Link href={ROUTES.HOME} className="flex items-center gap-1">
         <Image src="/images/site-logo.svg" width={23} height={23} alt="DevFlow logo" />
         <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
           Dev<span className="text-primary-500">Flow</span>
