0336a8e9043cbe66496ab7afe637164983f1ae6e
Jose Guerrero <josealberto.guerreroayala@gmail.com>
2026-02-14 01:24:00 -0500

    feat: implement LocalSearch component and enhance HomePage layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,35 @@
 import { Button } from "@/components/ui/button";
-import { signOut } from "@/auth";
 import ROUTES from "@/constants/routes";
+import Link from "next/link";
+import LocalSearch from "@/components/search/LocalSearch";
 
-const Page = () => {
+const HomePage = () => {
   return (
-    <main className="p-8">
-      <h1 className="mb-4 text-4xl font-bold">Titulo con Inter</h1>
-      <h2 className="font-space-grotesk text-4xl font-bold">Titulo con Space Grotesk</h2>
-      <form
-        className="px-10 pt-25"
-        action={async () => {
-          "use server";
+    <>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
 
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
-    </main>
+        <Button className="primary-gradient text-light-900! min-h-11.5 px-4 py-3" asChild>
+          <Link href={ROUTES.ASK_QUESTION}>Ask a question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">
+        <LocalSearch
+          route={ROUTES.HOME}
+          imgSrc="/icons/search.svg"
+          placeholder="Search Questions..."
+          otherClasses="flex-1"
+          iconPosition={"left"}
+        />
+      </section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 2</p>
+        <p>Question Card 3</p>
+      </div>
+    </>
   );
 };
 
-export default Page;
+export default HomePage;
